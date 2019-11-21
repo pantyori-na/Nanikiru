4b49ced835da5e215b96f2c5014bae45acb49dff
pantyori-na <kouzu.keinaka8@gmail.com>
2019-11-21 11:16:56 +0000

    start_card

--- app/controllers/site/post_images_controller.rb
+++ app/controllers/site/post_images_controller.rb
@@ -1,10 +1,10 @@
 class Site::PostImagesController < Site::Base
   before_action :set_post_image, only: [:show, :edit, :update, :destroy]
   def index
-    @post_images = PostImage.page(params[:page]).reverse_order.per(5)
-    @post_4p_images = PostImage.where(image_type: "player_4").page(params[:page]).reverse_order.per(5)
-    @post_3p_images = PostImage.where(image_type: "player_3").page(params[:page]).reverse_order.per(5)
-    @post_other_images = PostImage.where(image_type: "others").page(params[:page]).reverse_order.per(5)
+    @post_images = PostImage.page(params[:page]).reverse_order.per(10)
+    @post_4p_images = PostImage.where(image_type: "player_4").page(params[:page]).reverse_order.per(10)
+    @post_3p_images = PostImage.where(image_type: "player_3").page(params[:page]).reverse_order.per(10)
+    @post_other_images = PostImage.where(image_type: "others").page(params[:page]).reverse_order.per(10)
   end
 
   def index_all
